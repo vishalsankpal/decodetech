8ce79e080b0170155d2abc4bbd1fcbb9d148f716
vishal sankpal <vsakpal78@gmail.com>
2019-03-22 23:37:46 +0530

    class work

---
+++ js/classscript.js
@@ -0,0 +1,15 @@
+$(function(){
+  $(".visionfitness").hide(1000).show(1000);
+  });
+$(function(){
+  $(".menu").hide(1000).show(1000);
+  });
+
+
+/*----typing effect-----*/
+function warn()
+{
+    alert("Data is submited");
+    document.write("Data submitted successfully");
+}
+
