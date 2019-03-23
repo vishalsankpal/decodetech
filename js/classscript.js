ad30eb9b4420cd2b88b369a785405f01eed2fec2
vishal sankpal <vsakpal78@gmail.com>
2019-03-23 13:15:19 +0530

    changes

--- js/classscript.js
+++ js/classscript.js
@@ -1,15 +1,8 @@
+$(document).ready(function(){
 $(function(){
   $(".visionfitness").hide(1000).show(1000);
-  });
-$(function(){
   $(".menu").hide(1000).show(1000);
   });
+})
 
-
-/*----typing effect-----*/
-function warn()
-{
-    alert("Data is submited");
-    document.write("Data submitted successfully");
-}
 
